76abf14eb1610c56aff80af3cf24e5c223afac90
duvallwh <whduvall@gmail.com>
2018-03-30 11:16:21 -0700

    commit 2

--- automate_data_gather.py
+++ automate_data_gather.py
@@ -1,29 +1,35 @@
+"""Run the scs code on Raspberry Pi using python
+    * goal is to run via cron on Raspberry Pi
+    * break up queries so system does not freeze
+
+"""
+
 from subprocess import call, check_output
-
-
-# change the list
 
 def osio_query(year='2018', month='02', start_day='01', end_day='03', gases=True):
 
+    ## Section to check and process inputs
+    # needs to be tested
     if isinstance(year, int):
         year = str(year)
 
-    # needs to be tested
     def check_month_or_day(val):
-
         if isinstance(val, int):
             if val > 9:
                 val = str(val)
             else:
                 val = '0' + str(val)
-
         return val
 
-    for x in [month, start_day, end_day]:
-        check_month_or_day(x)
-
+    month = check_month_or_day(month)
+    start_day = check_month_or_day(start_day)
+    end_day = check_month_or_day(end_day)
+    
     def make_date(y,m,d):
-        '2018-02-04T00:00:00+00:00'
+        return y + '-' + m + '-' + d + 'T00:00:00+00:00'
+
+    start_date = make_date(y=year, m=month, d=start_day)
+    end_date = make_date(y=year, m=month, d=end_day)
         
     query_particulates = '/orgs/south-coast-science-test/san-diego/loc/1/particulates'
     query_gases = '/orgs/south-coast-science-test/san-diego/loc/1/gases'
@@ -31,10 +37,30 @@ def osio_query(year='2018', month='02', start_day='01', end_day='03', gases=True
     arg_list = ['./osio_topic_history.py',
     query_gases,
     '-s',
-    '2018-02-04T00:00:00+00:00',
+    start_date,
     '-e',
-    '2018-02-04T00:01:00+00:00']
+    end_date]
 
     #'> parts_2018_2_4-10.txt']
+    print(arg_list)
 
-    x = check_output(arg_list)
+    # x = check_output(arg_list)
+
+def big_osio_query(year=None, month=None, start_day=None, end_day=None):
+    if year < 2017:
+        return 'Bad year value'
+
+    day_list = list(range(start_day, end_day + 1))
+    for x in day_list:
+        osio_query(year=year, month=month, start_day=x, end_day=x+1, gases=True)
+        
+
+
+
+
+
+
+
+
+
+
